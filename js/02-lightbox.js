8aa7d44a8a9f792516447c4f6aa012638ce812fd
PawelRogowski <pawel.rogowski.praca@gmail.com>
2022-12-15 11:13:08 +0100

    added simpleLightbox task + minor changes/code cleaning

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryElement = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const linkElement = document.createElement("a");
+  linkElement.classList.add("gallery__item");
+  linkElement.setAttribute("href", item.original);
+
+  const imageElement = document.createElement("img");
+  imageElement.classList.add("gallery__image");
+  imageElement.src = item.preview;
+  imageElement.setAttribute("alt", item.description);
+
+  linkElement.appendChild(imageElement);
+  galleryElement.appendChild(linkElement);
+});
+
+const lightbox = new SimpleLightbox(".gallery .gallery__item", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionPosition: 250,
+});
